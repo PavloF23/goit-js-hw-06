9492a90ce5ab00e1775ab21d1dd5a867d12555a5
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-10-10 20:05:20 +0300

    101020222005

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+let ulList = document.getElementById("ingredients");
+// console.log(ulList);
+const foodIngredients = ingredients.forEach(ingredient => {
+  let items = document.createElement("li");
+  // console.log(items);
+  items.innerHTML = ingredient;
+  ulList.append(items);
+});
+
+console.log(foodIngredients);
