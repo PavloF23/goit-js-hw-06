8c9336985a1d9f318598e03c0f7886f4d7f4e4db
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-10-11 14:01:19 +0300

    140111102022

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+let inputVal = document.getElementById("validation-input");
+
+let totalLenght = inputVal.getAttribute("data-length");
+// console.log(totalLenght);
+let intTotalLenght = parseInt(totalLenght, 10);
+
+inputVal.oninput = function() {
+  if (inputVal.value.length === intTotalLenght) {
+    inputVal.classList.remove("invalid");
+    inputVal.classList.add("valid");
+  }
+  if (inputVal.value.length === 0) {
+    inputVal.classList.remove("valid");
+    inputVal.classList.remove("invalid");
+  }
+  if (inputVal.value.length !== intTotalLenght && inputVal.value.length !== 0) {
+    inputVal.classList.add("invalid");
+  }
+};
