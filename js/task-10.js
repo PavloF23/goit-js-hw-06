1f951cb24504501d88af3898862015880f761139
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-10-13 22:31:03 +0300

    191020222230

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,50 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const refs = {
+  createDiv: document.querySelector('[data-create]'),
+  destroyDiv: document.querySelector('[data-destroy]'),
+};
+
+// refs.createDiv.addEventListener('click', createBoxes);
+
+
+document.addEventListener('click', function() {
+  let div = document.createElement('div');
+  div.id = 'boxes';
+  div.innerHTML = 'Hi there!';
+  div.className = 'border pad';
+  div.style.color = getRandomHexColor();
+  div.style.width = 30;
+  div.style.height = 30;
+
+  document.body.appendChild(div);
+}, false);
+
+// console.log(createBoxes)
